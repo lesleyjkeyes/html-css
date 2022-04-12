9f49a7c2e2a412d6cedf946269892f42788c1146
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-04-11 20:28:08 -0500

    copied the code from in class assignment

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("testing html-css")
+// SNEAK PEEK
+// document.querySelector('article').innerHTML = 'From JS';
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  console.log("You submitted the form!");
+})
